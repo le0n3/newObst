3b0222e06aea54a8179d477bacaf8d6dc46f2b71
Leon <79692788+le0n3@users.noreply.github.com>
2024-01-13 14:53:51 +0100

    Zitate Cogs hinzugefügt

--- ObstBot/Cogs/zitate.py
+++ ObstBot/Cogs/zitate.py
@@ -1,3 +1,29 @@
 #ToDO Zitate
 #   addZitat
 # 	Zitat
+import discord
+from discord.ext import commands
+import logging
+from Helper import *
+
+class Random(commands.Cog):
+    def __init__(self, bot):
+        self.bot: commands.Bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Lodad Random Comands")
+
+    @commands.command(brief='Random RGB Wert', description='Erstelle eine Random RGB Farbe')
+    async def randomRGB(self, ctx: discord.ext.commands.Context):
+
+        await ctx.send(random_rgb(),delete_after= 15)
+        await ctx.message.delete()
+        logging.info(f"Random RGB wurde ausgeführt")
+
+
+
+
+async def setup(bot):
+    print("Loading Random Cog ...")
+    await bot.add_cog(Random(bot))
