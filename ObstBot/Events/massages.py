201aefd3ba4a2228a0f7c4ec191b612f414798a9
Leon <79692788+le0n3@users.noreply.github.com>
2024-01-16 11:09:20 +0100

    Massages und Reactions hinzugefügt

--- ObstBot/Events/massages.py
+++ ObstBot/Events/massages.py
@@ -1,10 +1,12 @@
+import random
+import discord
 from discord.ext import commands
 from discord.ext.commands import Cog
 
 
 class on_Messages_Evenets(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: commands.Bot = bot
         self._last_member = None
 
     @commands.Cog.listener()
@@ -12,10 +14,26 @@ class on_Messages_Evenets(commands.Cog):
         print("Lodad on_messags Events")
 
     @Cog.listener("on_message")
-    async def greet(self, message):
-        Cheers = ["Hi", "hi", "Hello", "hello"]
-        if message.content in Cheers:
-            pass
+    async def christoph(self, message: discord.message):
+        if message.content in "christoph" and message.guild:
+            if message.author != self.bot.user:
+                r = random.randint(1, 3)
+                if r == 1:
+                    await message.channel.send("Chillig Chillig Chillig", delete_after=40)
+
+    @Cog.listener("on_message")
+    async def LOL(self, message: discord.message):
+        if message.content == "LOL" and message.guild:
+            if message.author != self.bot.user:
+                await message.channel.send("LOL !?!", delete_after=40)
+
+    @Cog.listener("on_message")
+    async def absimmung(self, message: discord.message):
+        if message.channel == self.bot.get_channel(978186732363210803) and message.author != self.bot.user:
+            x = '\U0000274C'
+            check = '\U00002705'
+            await message.add_reaction(x)
+            await message.add_reaction(check)
 
 
 async def setup(bot):
